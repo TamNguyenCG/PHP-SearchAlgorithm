2e8f01fce751ca4977beb5f500b2fd05947e0b54
TamNguyen <ngductam91@gmail.com>
2021-05-20 11:40:03 +0700

    Classify phone number

--- PhoneNumber-Classification/index.php
+++ PhoneNumber-Classification/index.php
@@ -32,11 +32,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $length = strlen($input);
     $limitHigh = 10;
     $limitLow = 9;
-    if(empty($input)){
-        echo "<h2>Wrong type of phone number</h2>";
+    $flag = false;
+    $message = "<h2>Wrong type of phone number</h2>";
+
+    if (empty($input)) {
+        $flag = false;
         return;
-    }elseif($length > $limitHigh || $length < $limitLow){
-        echo "<h2>Wrong type of phone number</h2>";
+    } elseif ($length > $limitHigh || $length < $limitLow) {
+        $flag = false;
         return;
     }
     $viettel = ["0162", "0163", "0164", "0165", "0166", "0167", "0168", "0169", "032", "033", "034", "035", "036", "037", "038", "039"];
@@ -47,27 +50,36 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $isVina = [];
     $phone = explode(",", $input);
     foreach ($phone as $item) {
-        $substr1 = substr($item, 1, 3);
-        $substr2 = substr($item,1,2);
+        $substr1 = substr($item, 0, 4);
+        $substr2 = substr($item, 0, 3);
+
         for ($i = 0; $i < count($viettel); $i++) {
             if ($substr1 == $viettel[$i] || $viettel[$i] == $substr2) {
                 array_push($isViettel, $item);
+                $flag = true;
             }
         }
         for ($i = 0; $i < count($mobiphone); $i++) {
-            if ($substr1 == $mobiphone[$i] || $viettel[$i] == $substr2) {
+            if ($substr1 == $mobiphone[$i] || $mobiphone[$i] == $substr2) {
                 array_push($isMobi, $item);
+                $flag = true;
             }
         }
         for ($i = 0; $i < count($vinaphone); $i++) {
             if ($substr1 == $vinaphone[$i] || $vinaphone[$i] == $substr2) {
                 array_push($isVina, $item);
+                $flag = true;
             }
         }
     }
-    echo "Viettel's Phone number is :" . implode(", ", $isViettel)."<br>";
-    echo "Mobiphone's Phone number is :" . implode(", ", $isMobi)."<br>";
-    echo "Vinaphone's Phone number is :" . implode(", ", $isVina)."<br>";
+
+    if ($flag == false) {
+        echo $message;
+    }
+
+    echo "Viettel's Phone number is: " . implode(", ", $isViettel) . "<br>";
+    echo "Mobiphone's Phone number is: " . implode(", ", $isMobi) . "<br>";
+    echo "Vinaphone's Phone number is: " . implode(", ", $isVina) . "<br>";
 }
 ?>
 </body>
